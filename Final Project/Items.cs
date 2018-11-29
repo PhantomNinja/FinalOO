7c40c00c6dbad468738671a8cb3137f8b3d004a1
Aaron <aaronoakland1999@gmail.com>
2018-11-29 15:12:41 -0600

    Sales class started

--- Final Project/Items.cs
+++ Final Project/Items.cs
@@ -8,5 +8,14 @@ namespace Final_Project
 {
     class Items
     {
+        string Name;
+        string Location;
+        double Price;
+        int inStock;
+
+        public void PrintInfo()
+        {
+
+        }
     }
 }
---
+++ Final Project/Sales.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Project
+{
+    class Sales
+    {
+        int In_Stock;
+        int Sold_This_Year;
+        int Sold_Last_Year;
+        int Total_Sold;
+
+        public void UpdateStock(int In_Stock, int Sold_This_Year, int Total_Sold, int Sold)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NewYear(int Sold_This_Year, int Sold_Last_Year)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Print_Stats()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
